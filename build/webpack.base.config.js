7202d7e09a4b95c6c4d465d6c61509417b64abc0
chping2125 <chping_2125@163.com>
2017-01-11 09:48:03 +0800

    webpack配置理解

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -22,7 +22,12 @@ module.exports = {
     publicPath: '/dist/',
     filename: '[name].[chunkhash].js'
   },
+  //resolve 用来配置文件路径的指向。
+  //可以定义文件跟模块的默认路径及后缀等，节省 webpack 搜索文件的时间、优化引用模块时的体验。
+  //常用的包括alias、extensions、root、modulesDirectories属性
+  //可以参考 http://www.ferecord.com/webpack-summary.html#resolve
   resolve: {
+    //alias：是个对象，把资源路径重定向到另一个路径
     alias: {
       'public': path.resolve(__dirname, '../public')
     }
